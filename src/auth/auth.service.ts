0996765ba063c7ee4e898de01b06be579ea92bf7
Ssejjemba Dan <ssejjembadan@gmail.com>
2022-11-09 19:19:14 +0300

    Add creating user to auth module

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Auth } from './auth.entity';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(@InjectRepository(Auth) private repo: Repository<Auth>) {}
+
+  create(email: string, password: string) {
+    const user = this.repo.create({ email, password });
+
+    return this.repo.save(user);
+  }
+}
