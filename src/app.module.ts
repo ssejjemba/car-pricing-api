848da79608825b78c94c2574d6a78af080b34008
Ssejjemba Dan <ssejjembadan@gmail.com>
2022-11-09 17:16:16 +0300

    Initialise empty modules for the auth and reports
    These are the two main services for this application

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { ReportsModule } from './reports/reports.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, ReportsModule],
   controllers: [AppController],
   providers: [AppService],
 })
