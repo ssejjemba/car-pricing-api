6f9561af3deb0c180bc31a3744700ee058700056
Ssejjemba Dan <ssejjembadan@gmail.com>
2022-11-09 17:42:26 +0300

    (database): Initialise typeorm with sqlite database
    - will be swapped later for postgres

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,21 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { ReportsModule } from './reports/reports.module';
 
 @Module({
-  imports: [AuthModule, ReportsModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: 'db.sqlite',
+      entities: [],
+      synchronize: true,
+    }),
+    AuthModule,
+    ReportsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
